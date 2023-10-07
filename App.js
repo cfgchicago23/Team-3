97260ebe3f4aa110e74d5ad25b4d09ea82b424ed
meghna2409 <meghna.jayaraj24@gmail.com>
2023-10-06 23:40:31 -0500

    Finished basic landing page for participants

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, ImageBackground, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
 import Home from './app/home'
+import ParticipantLandingPage from '.app/ParticipantLandingPage'
 
 export default function App() {
   return (
-    <Home />  // Render the Home component
+    <ParticipantLandingPage />
+    //<Home />  // Render the Home component
   );
 }
 
