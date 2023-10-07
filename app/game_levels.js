648cbe65a08f9ca8d55618f4b07e7dba28691ae1
wendytu2022 <73412642+wendytu2022@users.noreply.github.com>
2023-10-07 02:30:18 -0400

    Added comments

--- app/game_levels.js
+++ app/game_levels.js
@@ -10,35 +10,66 @@ export default function App() {
     //<View>
       <View style={styles.game_title}>
         <Text style={styles.game_title}>Thrive Challenge</Text>
+        {/* Rows of buttons to different levels of the game */}
+        {/* -------------------------- ROW 1 --------------------------- */}
         <View style={styles.screen1}>
           <TouchableOpacity
             onPress={button1ClickedHandler}
-            style={styles.button}>
+            style={styles.button1}>
             <Text>I'm a button</Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={button1ClickedHandler}
-            style={styles.button}>
-            <Text>I'm another button</Text>
-          </TouchableOpacity>
-        <View style={styles.screen2}>
-          <TouchableOpacity
-            onPress={button1ClickedHandler}
-            style={styles.button}>
-            <Text>I'm a button</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={button1ClickedHandler}
-            style={styles.button}>
+            style={styles.button2}>
             <Text>I'm another button</Text>
           </TouchableOpacity>
         </View>
+         {/* -------------------------- ROW 2 --------------------------- */}
+        <View style={styles.screen2}>
+          <TouchableOpacity
+            onPress={button1ClickedHandler}
+            style={styles.button1}>
+            <Text>I'm a button</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={button1ClickedHandler}
+            style={styles.button2}>
+            <Text>I'm another button</Text>
+          </TouchableOpacity>
+       </View>
+        {/* -------------------------- ROW 3 --------------------------- */}
+       <View style={styles.screen3}>
+          <TouchableOpacity
+            onPress={button1ClickedHandler}
+            style={styles.button1}>
+            <Text>I'm a button</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={button1ClickedHandler}
+            style={styles.button2}>
+            <Text>I'm another button</Text>
+          </TouchableOpacity>
+       </View>
+        {/* -------------------------- ROW 4 --------------------------- */}
+       <View style={styles.screen4}>
+          <TouchableOpacity
+            onPress={button1ClickedHandler}
+            style={styles.button1}>
+            <Text>I'm a button</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={button1ClickedHandler}
+            style={styles.button2}>
+            <Text>I'm another button</Text>
+          </TouchableOpacity>
        </View>
       </View>
   );
 }
 
+// <----------------------- STYLING --------------------------->
 const styles = StyleSheet.create({
+  // Game title styling
   game_title: {
     marginTop: '7%',
     fontSize: 40,
@@ -50,6 +81,7 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     textDecorationColor: 'pink',
   },
+  // Row 1 Positioning
   screen1: {
     marginTop: '20%',
     position: 'absolute',
@@ -58,7 +90,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
   },
-  button: {
+  // Row 2 Positioning
+  screen2: {
+    marginTop: '60%',
+    position: 'absolute',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  // Row 3 Positioning
+  screen3: {
+    marginTop: '100%',
+    position: 'absolute',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  // Row 4 Positioning
+  screen4: {
+    marginTop: '140%',
+    position: 'absolute',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  button1: {
     margin: 'auto',
     width: 120,
     height: 120,
@@ -70,13 +129,36 @@ const styles = StyleSheet.create({
     backgroundColor: 'pink',
     flexDirection: 'row',
   },
-  screen2: {
-    marginTop: '60%',
-    position: 'absolute',
-    flex: 1,
-    justifyContent: 'flex-start',
+  button2: {
+    width: 120,
+    height: 120,
+    justifyContent: 'space-between',
     alignItems: 'center',
+    padding: 10,
+    margin: '5%',
+    borderRadius: 100,
+    backgroundColor: 'pink',
     flexDirection: 'row',
   },
-  
+  button3: {
+    margin: 'auto',
+    width: 120,
+    height: 120,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    margin: '5%',
+    borderRadius: 100,
+    backgroundColor: 'pink',
+  },
+  button4: {
+    width: 120,
+    height: 120,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    margin: '5%',
+    borderRadius: 100,
+    backgroundColor: 'pink',
+  },
 });
