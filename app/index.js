dab099e34c4cdbedfd246e11aaa8b26f081cf133
wendytu2022 <73412642+wendytu2022@users.noreply.github.com>
2023-10-07 08:28:43 -0400

    Moved contents of home.js to index.js for testing

--- app/index.js
+++ app/index.js
@@ -1,98 +1,90 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import EvaluationFormComponent from '../components/EvaluationForm';
-import RegisterClubComponent from '../components/RegisterClub';
-import RegisterParticipantComponent from '../components/RegisterParticipant';
+import React from 'react';
+import { View, Text, ImageBackground, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { Link } from 'expo-router';
 
-function ManagementPage() {
-  const [selectedComponent, setSelectedComponent] = useState(null);
-
-  const handleBack = () => {
-    setSelectedComponent(null); 
-  };
-``
-  const renderSelectedComponent = () => {
-    switch (selectedComponent) {
-      case 'registerParticipant':
-        return <RegisterParticipantComponent onBack={handleBack} />;
-      case 'registerClub':
-        return <RegisterClubComponent onBack={handleBack} />;
-      case 'evaluationForm':
-        return <EvaluationFormComponent onBack={handleBack} />;
-      default:
-        return null;
-    }
-  };
-
-  if (selectedComponent) {
-    return renderSelectedComponent();
-  }
-
+function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={styles.headerText}>Management Page</Text>
-      <TouchableOpacity style={styles.button} onPress={() => setSelectedComponent('registerParticipant')}>
-        <Text style={styles.buttonText}> Register a Participant</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => setSelectedComponent('registerClub')}>
-        <Text style={styles.buttonText}> Register Your Club</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => setSelectedComponent('evaluationForm')}>
-        <Text style={styles.buttonText}> Fill Out Evaluation Form</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={require('../assets/background.png')} style={styles.backgroundImage}>
+        {/* Header */}
+        <Text style={styles.headerText}>ThriveForGirls</Text>
+
+        {/* Main Content */}
+        <Text style={styles.title}>Love. Create. Share.</Text>
+        
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.description}>
+            Empower provides young girls with the tools they need to build healthy and lasting friendships.{"\n"}
+          </Text>
+          <Text style={styles.description}>
+            We work with girls to help them rise up beyond their circumstances, become role models in their communities, and learn how to recognize abuse and exploitation. 
+          </Text>
+        </View>
+
+        {/* Login Button */}
+        <TouchableOpacity zstyle={styles.loginButton}>
+            <Link href="/game_levels">Game Levels (TESTING)</Link>
+        </TouchableOpacity>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#f8f8f8'
   },
-  title: {
-    fontSize: 28,
-    fontWeight: '700',
-    marginBottom: 30,
-    textAlign: 'center'
+  backgroundImage: {
+    flex: 1,
+    paddingHorizontal: 30, // Increased padding 
+    justifyContent: 'center',
   },
   headerText: {
-    fontSize: 32,
-    fontWeight: '700',
-    marginBottom: 50,
-    color: '#34495E',
-    textAlign: 'center'
+    fontSize: 36,
+    fontWeight: '900', // Bolder
+    textAlign: 'center',
+    marginTop: 60, // More spacing
   },
-  input: {
-    backgroundColor: '#fff',
-    padding: 15,
-    borderRadius: 10,
-    width: '100%',
-    marginBottom: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
+  title: {
+    fontSize: 30,
+    fontWeight: '700', 
+    textAlign: 'center',
+    marginTop: 60,
   },
-  button: {
-    backgroundColor: '#E74C3C',
+  description: {
+    fontSize: 16,
+  },
+  descriptionContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',  // Semi-transparent white
+    padding: 20,  // Padding around the text
+    borderRadius: 10,  // Rounded corners
+    marginTop: 30,  // Some spacing from the title
+    marginBottom: 30,  // Some spacing before the button
+  },
+  loginButton: {
+    marginTop: 90,
+    backgroundColor: '#E74C3C',  // Slightly different shade of red
     padding: 16,
-    borderRadius: 50,
-    width: '80%',
+    borderRadius: 50,  // Full rounded edges
     alignItems: 'center',
+    alignSelf: 'center',
+    width: '60%',  // Less width
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    marginBottom: 20
+    position: 'absolute',
+    top: '50%',
+
   },
-  buttonText: {
-    color: '#ffffff',
-    fontWeight: '600',
-    fontSize: 18
+  loginText: {
+    color: '#fff',
+    fontWeight: '600',  // Slightly less bold
+    fontSize: 18  // Bigger font size
   },
 });
 
-export default ManagementPage;
+export default Home;
+
+
