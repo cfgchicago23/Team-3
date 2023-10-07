b171fb3d4ab049b312d8ad3b702d7277786e97d6
wendytu2022 <73412642+wendytu2022@users.noreply.github.com>
2023-10-06 22:34:34 -0400

    Explored adding progress circle; ultimately decided to not use App.js in favor of index.js

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { Link } from 'expo-router';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <Link href="/game_levels">Play Game!</Link>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+// import { AnimatedCircularProgress } from 'react-native-circular-progress';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hewwo?</Text>
+//       <StatusBar style="auto" />
+
+//       <AnimatedCircularProgress
+//         size={200}
+//         width={3}
+//         fill={this.state.fill}
+//         tintColor="#00e0ff"
+//         backgroundColor="#3d5875">
+//         {
+//           (fill) => (
+//             <Text>
+//               { this.state.fill }
+//             </Text>
+//           )
+//         }
+//       </AnimatedCircularProgress>
+//     </View>               
+//   );}
