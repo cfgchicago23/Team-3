6cf255fecdb43d8ec3a2a778ee90e25e9b9f53e8
wendytu2022 <73412642+wendytu2022@users.noreply.github.com>
2023-10-07 10:15:13 -0400

    Added answers and correct answer for one question

---
+++ app/test_answers.js
@@ -0,0 +1,11 @@
+export default data = [
+    {
+      question: "What should Jan do?",
+      options: [
+        "Be angry at herself - she isn't worth anyone's time.",
+        "Be fearful of him - maybe she shouldn't have talked back to him.",
+        "Be proud - she stood up for herself and showed the person that she is strong.",
+      ],
+      correct_option: "Be proud - she stood up for herself and showed the person that she is strong",
+    },
+  ];
