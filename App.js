c136554324ff0f7813b9e34e9792854f1d62505a
Madhumitha Shetty <madhumithashetty@MacBook-Air-53.local>
2023-10-06 21:37:19 -0500

    Basic help page

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  HelpPage  from './app/Help'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style = {styles.container}>
+      <StatusBar style="auto"/>
+      <HelpPage/>
+     </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
   );
 }
 
