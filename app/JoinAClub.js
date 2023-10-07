4ea6306b96259bf29564a2121cb632a9f83eecc1
meghna2409 <meghna.jayaraj24@gmail.com>
2023-10-07 00:48:07 -0500

    Added join a club page for participants

---
+++ app/JoinAClub.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+
+function JoinAClub() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleJoin = () => {
+    // Handle the join logic, for example, sending data to the server.
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Phone Number:', phoneNumber);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.headerText}>Join a Club!</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+        keyboardType="phone-pad"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleJoin}>
+        <Text style={styles.buttonText}>Join</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f8f8f8'
+  },
+  headerText: {
+    fontSize: 32,
+    fontWeight: '700',
+    marginBottom: 50,
+    color: '#34495E',
+    textAlign: 'center'
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    width: '100%',
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  button: {
+    backgroundColor: '#E74C3C',
+    padding: 16,
+    borderRadius: 50,
+    width: '100%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 18
+  },
+});
+
+export default JoinAClub;
